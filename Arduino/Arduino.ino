65c470534267ceda46b3af0acdd8ddc3128e934d
אלון עמרם <elonamrammm1234@gmail.com>
2025-02-04 11:24:05 +0200

    added sensor LDR and read value analog

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,64 +1,77 @@
 
-#define a1a 18
-#define a1b 19
+// #define a1a 18
+// #define a1b 19
 
-unsigned long lastCheck=0;
-const unsigned long Minutes=(1000 * 60)*10;
+// unsigned long lastCheck=0;
+// const unsigned long Minutes=(1000 * 60)*10;
 
+
+
+int lightLDR=39;
 
 //------------state mode-----------
-#define Mode_Temperature 100
-#define Mode_SoilHumidity 101
-#define Mode_Shabbat 102
-#define Mode_Manual 103
+// #define Mode_Temperature 100
+// #define Mode_SoilHumidity 101
+// #define Mode_Shabbat 102
+// #define Mode_Manual 103
+
+// int modeWork;
+
 
 
 void setup() {
   Serial.begin(115200);
 
-  dht.begin(); 
-
+  // dht.begin();
   // WiFi_SETUP();
-  pinMode(a1a,OUTPUT);
-  pinMode(a1b,OUTPUT);
+  // pinMode(a1a,OUTPUT);
+  // pinMode(a1b,OUTPUT);
 
 }
 
 void loop() {
   // sendData();
+int light=analogRead(lightLDR);
+int mapValue = map(light, 0, 4095, 0, 255);
+Serial.println(mapValue);
+delay(500);
 
-  if(millis()-lastCheck > Minutes){
-    lastCheck=millis();
-    Serial.println("check mode work from server");
-    switch(modeWork){
-        case Mode_Temperature:
-            Serial.println("Mode: Temperature");
-            break;
-        case Mode_SoilHumidity:
-            Serial.println("Mode: Soil Humidity");
-            break;
-        case Mode_Shabbat:
-            Serial.println("Mode: Shabbat");
-            break;
-        case Mode_Manual:
-            Serial.println("Mode: Manual");
-            break;
-        default:
-            Serial.println("Unknown mode, switching to Manual");
-            modeWork = Mode_Manual;
-            break;
-    }
-  }
+
+
+
+//   if(millis()-lastCheck > Minutes){
+//     lastCheck=millis();
+//     Serial.println("check mode work from server");
+//   }
+//
+//   switch(modeWork){
+//         case Mode_Temperature:
+//             Serial.println("Mode: Temperature");
+//             break;
+//         case Mode_SoilHumidity:
+//             Serial.println("Mode: Soil Humidity");
+//             break;
+//         case Mode_Shabbat:
+//             Serial.println("Mode: Shabbat");
+//             break;
+//         case Mode_Manual:
+//             Serial.println("Mode: Manual");
+//             break;
+//         default:
+//             Serial.println("Unknown mode, switching to Manual");
+//             modeWork = Mode_Manual;
+//             break;
+//     }
   
 }
 
 
-function pumpOn(){
-    digitalWrite(a1a,HIGH);
-    digitalWrite(a1b,LOW);
-}
-
-function pumpOff(){
-  digitalWrite(a1a,HIGH);
-  digitalWrite(a1b,HIGH);
-}
+// function pumpOn(){
+//     digitalWrite(a1a,HIGH);
+//     digitalWrite(a1b,LOW);
+// }
+//
+// function pumpOff(){
+//   digitalWrite(a1a,HIGH);
+//   digitalWrite(a1b,HIGH);
+// }
--- Arduino/K10_wifiClient.ino
+++ Arduino/K10_wifiClient.ino
@@ -1,57 +1,57 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <HTTPClient.h>
+// #include <WiFi.h>
+// #include <WiFiClient.h>
+// #include <HTTPClient.h>
 
 
-const char* ssid = "0000";
-// const char* password = "0000";
+// const char* ssid = "0000";
+// // const char* password = "0000";
 
-WiFiClient Client;
+// WiFiClient Client;
 
-void WiFi_SETUP(){
-  WiFi.begin(ssid);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-}
+// void WiFi_SETUP(){
+//   WiFi.begin(ssid);
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     Serial.print(".");
+//   }
+//   Serial.println("");
+//   Serial.println("WiFi connected");
+// }
 
 
-void sendData(){
-  HTTPClient http;
-  http.begin(Client, "http://10.9.1.91:4000/esp");
+// void sendData(){
+//   HTTPClient http;
+//   http.begin(Client, "http://10.9.1.91:4000/esp");
   
-  int httpCode = http.GET();
-  if(httpCode == HTTP_CODE_OK) {
-    Serial.print("HTTP response code ");
-    Serial.println(httpCode);
-    // String Res = http.getString();
-    //Serial.println(Res);
-    //  ret = Res.toInt();
-    }
-    http.end();
-}
-
-
-// int GetData() {
-//    int ret = -1;
-//    HTTPClient http;
-//    String dataURL = "";
-//    dataURL += "IDY=";
-//    dataURL += IDY;
-//    http.begin(client, "http://192.168.1.83:4000/ESP/Pull?" + dataURL);
-//    int httpCode = http.GET();
-//    //Serial.println(httpCode);
-//    if(httpCode == HTTP_CODE_OK) {
-//      //Serial.print("HTTP response code ");
-//      //Serial.println(httpCode);
-//      String Res = http.getString();
-//      //Serial.println(Res);
-//      ret = Res.toInt();
+//   int httpCode = http.GET();
+//   if(httpCode == HTTP_CODE_OK) {
+//     Serial.print("HTTP response code ");
+//     Serial.println(httpCode);
+//     // String Res = http.getString();
+//     //Serial.println(Res);
+//     //  ret = Res.toInt();
 //     }
 //     http.end();
-        
-//     return ret;
 // }
+
+
+// // int GetData() {
+// //    int ret = -1;
+// //    HTTPClient http;
+// //    String dataURL = "";
+// //    dataURL += "IDY=";
+// //    dataURL += IDY;
+// //    http.begin(client, "http://192.168.1.83:4000/ESP/Pull?" + dataURL);
+// //    int httpCode = http.GET();
+// //    //Serial.println(httpCode);
+// //    if(httpCode == HTTP_CODE_OK) {
+// //      //Serial.print("HTTP response code ");
+// //      //Serial.println(httpCode);
+// //      String Res = http.getString();
+// //      //Serial.println(Res);
+// //      ret = Res.toInt();
+// //     }
+// //     http.end();
+        
+// //     return ret;
+// // }
