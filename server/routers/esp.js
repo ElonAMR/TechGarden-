892a70b669bdf692b27c34521277cf95d1978529
אלון עמרם <elonamrammm1234@gmail.com>
2025-03-18 01:26:08 +0200

    import dataBase for ready use

--- server/routers/esp.js
+++ server/routers/esp.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs=require('fs');
+const db = require('../models/dataBase')
 
 router.get('/',async (req,res)=>{
     try{
