bdcce937cfc8b8eeb0f2168a02a7ca8a347fb6f0
אלון עמרם <elonamrammm1234@gmail.com>
2025-03-18 23:06:45 +0200

    create SHABBAT_MODE and last updated code

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -21,6 +21,9 @@ JsonDocument doc;
 int currentState;
 unsigned long lastCheckTime = 0;
 const unsigned long ONE_MINUTES = (1000 * 60); // convert milli-seconds to 1 min
+unsigned long DataPullTime=0;
+unsigned long activationTime=0;
+bool pumpPowerOn=false;
 
 //------------הגדרת משתנים מוד טמפרטורה---------------
 
@@ -28,11 +31,8 @@ float currentTemp;
 float tempServer;
 int minTime ,maxTime ;
 int light;
-bool pumpPowerOn=false;
 bool isMorning;
 int counterOnPump = 0;
-unsigned long DataPullTime=0;
-unsigned long activationTime=0;
 
 //------------הגדרת משתנים מוד לחות---------------
 
@@ -45,6 +45,8 @@ int currentHour;
 int firstStart;
 int secondStart;
 int wateringDuration;
+bool firstActivation = false;
+bool secondActivation = false;
 
 //------------הגדרת משתנים מוד ידני---------------
 
@@ -52,6 +54,7 @@ bool activatePump;
 
 
 
+//------------קוד---------------
 
 void setup() {
   Serial.begin(115200);
@@ -68,12 +71,16 @@ void loop() {
   if( (millis() - lastCheckTime) >= (ONE_MINUTES * 10) ){
     currentState = GetData();
     lastCheckTime = millis();
+    DataPullTime = 0;
+    activationTime = 0;
+    pumpOff();
+    pumpPowerOn = false;
     Serial.println("current State = "+ String(currentState));
   }
 
   switch(currentState){
-        case TEMP_MODE:
 
+        case TEMP_MODE:
           Serial.println("Mode: Temperature");
           currentTemp = dht.readTemperature();
           light = map(analogRead(lightSensor), 0, 4095, 0, 100);
@@ -111,11 +118,11 @@ void loop() {
           if (isMorning && counterOnPump == 2) {
                counterOnPump = 0;
           }
-
           break;
 
-        case SOIL_MOISTURE_MODE:
 
+
+        case SOIL_MOISTURE_MODE:
             Serial.println("Mode: Soil Humidity");
             deserializeJson(doc, getJsonData("soilHumidity"));
             Humidity = doc["desiredHumidity"];
@@ -129,21 +136,44 @@ void loop() {
             }
             break;
 
+
+
         case SHABBAT_MODE:
-
             Serial.println("Mode: Shabbat");
-            deserializeJson(doc, getJsonData("shabbat"));
-            currentHour = doc["currentTimeHour"];
-            firstStart = doc["firstStart"];
-            secondStart = doc["secondStart"];
-            wateringDuration = doc["wateringDuration"];
-
-
-
-
-
-
+            if (millis() - DataPullTime >= (2 * ONE_MINUTES) || DataPullTime == 0) {
+                deserializeJson(doc, getJsonData("shabbat"));
+                currentHour = String(doc["currentTimeHour"]).toInt();
+                firstStart = String(doc["firstStart"]).substring(0,2).toInt();
+                secondStart = String(doc["secondStart"]).substring(0,2).toInt();
+                wateringDuration = String(doc["wateringDuration"]).toInt();
+                DataPullTime = millis();
+            }
+            // בדיקה אם יש צורך להפעיל את המשאבה
+            if (currentHour == firstStart && !firstActivation && !pumpPowerOn) {
+                pumpOn();
+                pumpPowerOn = true;
+                activationTime = millis();
+                firstActivation = true;
+            }
+            else if (currentHour == secondStart && !secondActivation && !pumpPowerOn) {
+                pumpOn();
+                pumpPowerOn = true;
+                activationTime = millis();
+                secondActivation = true;
+            }
+            // כיבוי המשאבה לאחר משך זמן ההשקיה
+            if (pumpPowerOn && (millis() - activationTime >= (wateringDuration * ONE_MINUTES))) {
+                pumpOff();
+                pumpPowerOn = false;
+            }
+            // איפוס ההפעלות לשבת הבאה
+            if (currentHour == 0) {
+                firstActivation = false;
+                secondActivation = false;
+            }
             break;
+
+
 
         case MANUAL_MODE:
             Serial.println("Mode: Manual");
@@ -156,6 +186,8 @@ void loop() {
             }
             break;
 
+
+
         default:
             Serial.println("Unknown mode, switching to Manual");
             currentState = Mode_Manual;
@@ -165,6 +197,8 @@ void loop() {
 }
 
 
+
+//------------פונקציות להדלקת וכיבוי משאבה---------------
 
 function pumpOn(){
     digitalWrite(a1a,HIGH);
